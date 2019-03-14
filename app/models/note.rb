894964f79e7792a6cc669c54e62b5e8b79d1047e
Jacek Podkanski <ruby.object@googlemail.com>
2019-03-14 20:51:52 +0000

    annotate

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: notes
+#
+#  id         :integer          not null, primary key
+#  author     :string
+#  content    :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Note < ApplicationRecord
 end
--- test/models/note_test.rb
+++ test/models/note_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: notes
+#
+#  id         :integer          not null, primary key
+#  author     :string
+#  content    :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class NoteTest < ActiveSupport::TestCase
