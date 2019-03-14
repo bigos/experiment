46085539cb3283ed79f5cd41e699fd65349ac503
Jacek Podkanski <ruby.object@googlemail.com>
2019-03-14 22:47:46 +0000

    basic elm SPA works

--- vendor/elm_source/src/Main.elm
+++ vendor/elm_source/src/Main.elm
@@ -1,24 +1,84 @@
+module Main exposing (Model, Msg(..), main, subscriptions, update, view)
+
+import Browser exposing (..)
 import Html exposing (..)
+
+
+
+-- MAIN
+
+
+main =
+    Browser.document
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
 
 
 -- MODEL
 
-type alias Model = { ... }
+
+type alias Model =
+    { message : String
+    , code : Int
+    }
+
+
+type alias Flags =
+    --empty tuple
+    ()
+
+
+init : Flags -> ( Model, Cmd Msg )
+init flags =
+    ( { message = "Hi"
+      , code = 0
+      }
+    , Cmd.none
+    )
+
 
 
 -- UPDATE
 
-type Msg = Reset | ...
 
-    update : Msg -> Model -> Model
-    update msg model =
-        case msg of
-            Reset -> ...
-                     ...
+type Msg
+    = Reset
+    | Foo
+
+
+
+-- update : Msg -> Model -> ( Model, Cmd Msg )
+
+
+update msg model =
+    case msg of
+        Reset ->
+            ( model, Cmd.none )
+
+        Foo ->
+            ( model, Cmd.none )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
 
 
 -- VIEW
 
-view : Model -> Html Msg
+
 view model =
-  ...
+    { title = "My first title"
+    , body =
+        [ div [] [ text "hurray!!!" ]
+        ]
+    }
