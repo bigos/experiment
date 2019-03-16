5e3592e069d953fd0dc642979b4eec95d1862ef6
Jacek Podkanski <ruby.object@googlemail.com>
2019-03-16 15:44:22 +0000

    got my request working after cors issue

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,14 @@ module Experiment
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+
+    # https://medium.com/@Nicholson85/handling-cors-issues-in-your-rails-api-120dfbcb8a24
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
   end
 end
